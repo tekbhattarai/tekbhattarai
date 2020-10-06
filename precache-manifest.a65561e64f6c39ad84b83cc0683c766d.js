4e7d25f1fb6f1c52f96164378422034f5e30be0e
root <tbhatta2@oswego.edu>
2020-10-06 19:38:45 -0400

    Updates

---
+++ precache-manifest.a65561e64f6c39ad84b83cc0683c766d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98f8a0bf06f9c68efd1aeec662c8ba09",
+    "url": "/tekbhattarai/index.html"
+  },
+  {
+    "revision": "494301e75df9fc106844",
+    "url": "/tekbhattarai/static/css/main.8da6c54d.chunk.css"
+  },
+  {
+    "revision": "730eea5d523d4766dfb2",
+    "url": "/tekbhattarai/static/js/2.ccc197b9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tekbhattarai/static/js/2.ccc197b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "494301e75df9fc106844",
+    "url": "/tekbhattarai/static/js/main.400def51.chunk.js"
+  },
+  {
+    "revision": "daa87c1055c2da31f462",
+    "url": "/tekbhattarai/static/js/runtime-main.8fbd2edc.js"
+  }
+]);
